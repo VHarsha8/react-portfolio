4c8ec2283c9b3a2edae5bc6ebe9b86180d7929f6
Harshavardhan <140392137+VHarsha8@users.noreply.github.com>
2024-08-17 13:42:28 +0530

    Update Component1.jsx

--- src/Component1.jsx
+++ src/Component1.jsx
@@ -2,8 +2,8 @@
 function Combonent() {
     return (
       <div className='flex gap-1 justify-start'>
-                    <a href="mailto:harshavengala8@gmail.com" className="text-twitter px-4 py-1 bg-white font-mono font-medium text-lg rounded-l-full">Resume</a>
-                    <a href="https://drive.google.com/file/d/117_dh6fHTXicA2rNmC4u4IOSglFdmtBU/view?usp=sharing" className="text-white py-1 px-4 bg-black font-mono font-medium text-lg rounded-r-full">Contact</a>
+                    <a href="mailto:harshavengala8@gmail.com" className="text-twitter px-4 py-1 bg-white font-mono font-medium text-lg rounded-l-full">Contact</a>
+                    <a href="https://drive.google.com/file/d/117_dh6fHTXicA2rNmC4u4IOSglFdmtBU/view?usp=sharing" className="text-white py-1 px-4 bg-black font-mono font-medium text-lg rounded-r-full">Resume</a>
       </div>
     );
   }
