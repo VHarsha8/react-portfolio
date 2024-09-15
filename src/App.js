62b67950e04486d3565047aff870247acf10254e
VHarsha8 <harshavardhan6304@gmail.com>
2024-09-15 20:53:29 +0530

    redesign

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import Epistemicon from './Epistemicon';
 import EpistemiconHomePage from './EpistemiconHomePage';
 import Footer from './footer';
-import NavBar from './NavBar';
 import Tabs from './Tabs';
 import CseTabs from './CseTabs';
+import Navbar from './NavBar';
+import UpwardsComponent from './Epistemicon';
+
+
 
 
 
 function App() {
+  
   return (
+    
+
     <div className='relative flex flex-col'>
 
-      <div className='fixed z-20 m md:w-12 bottom-10 right-6 md:right-12'>
-      <a href="https://chat.whatsapp.com/Iw22bVdtbXs3iHtU9ppMTg" target='_blank'> <img src='whatsapp.svg' className='w-12 rounded-full'></img>
-    </a>
-      </div>
+    <div className='fixed z-20 m md:w-12 bottom-10 right-6 md:right-12'>
+    <a href="https://chat.whatsapp.com/Iw22bVdtbXs3iHtU9ppMTg" target='_blank'> <img src='whatsapp.svg' className='w-12 rounded-full'></img>
+  </a>
+    </div>
 
+    
+    <Navbar />
+    <EpistemiconHomePage />
       
-      <NavBar />
-      <EpistemiconHomePage />
-      
-         <Epistemicon />
+         <UpwardsComponent />
           <Tabs />
           <CseTabs />
        
          <Footer />
-
-
-
-        
-         
     </div>
   );
 }
--- src/Epistemicon.jsx
+++ src/Epistemicon.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 import { gsap } from 'gsap';
 
-function Epistemicon() {
+function UpwardsComponent() {
     const slides = [
         {
             url: 'EPISTEMICONPOSTER.jpg'
@@ -11,62 +11,48 @@ function Epistemicon() {
         {
             url: 'QUIZPOSTER.jpg'
         },
-        
-        
+        // Add more slides as needed
     ];
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const currentIndex = 0;
     const slideRef = useRef(null);
 
-    
     const animateSlide = (newIndex) => {
         gsap.to(slideRef.current, {
-            x: -newIndex * 100 + '%', 
-            duration: 0.3, 
-            ease: "power3.inOut", 
-            snap: { x: 1 }, 
+            x: -newIndex * 100 + '%',
+            duration: 0.3,
+            ease: "power3.inOut",
+            snap: { x: 1 },
         });
     };
 
     const prevSlide = () => {
         const isFirstSlide = currentIndex === 0;
         const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
         animateSlide(newIndex);
     };
 
     const nextSlide = () => {
         const isLastSlide = currentIndex === slides.length - 1;
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
         animateSlide(newIndex);
     };
 
     const goToSlide = (slideIndex) => {
-        setCurrentIndex(slideIndex);
         animateSlide(slideIndex);
     };
 
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            nextSlide();
-        }, 5000); 
-
-        return () => clearInterval(interval); 
-    }, [currentIndex]);
-
     return (
-        <div className='md:max-w-[1400px]  h-[400px] md:h-[580px] w-full m-auto   relative group pb-16 px-4 overflow-hidden'>
+        <div id="upwards" className="md:max-w-[1400px] h-[400px] md:h-[580px] w-full m-auto relative group pb-16 px-4 overflow-hidden">
             {/* Slide container with snapping */}
             <div
                 ref={slideRef}
-                className='flex w-full h-full transition-all duration-700 ease-in-out transform'
+                className="flex w-full h-full transition-all duration-700 ease-in-out transform"
             >
                 {slides.map((slide, index) => (
                     <div
                         key={index}
-                        className='flex-shrink-0 w-full h-full bg-center bg-cover rounded-2xl'
+                        className="flex-shrink-0 w-full h-full bg-center bg-cover rounded-2xl"
                         style={{ backgroundImage: `url(${slide.url})` }}
                     />
                 ))}
@@ -74,7 +60,7 @@ function Epistemicon() {
 
             {/* Left Arrow */}
             <div
-                className='absolute hidden group-hover:block top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer text-white'
+                className="absolute hidden group-hover:block top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer text-white"
                 onClick={prevSlide}
             >
                 <BsChevronCompactLeft size={30} />
@@ -82,14 +68,14 @@ function Epistemicon() {
 
             {/* Right Arrow */}
             <div
-                className='absolute hidden group-hover:block top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer text-white'
+                className="absolute hidden group-hover:block top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 cursor-pointer text-white"
                 onClick={nextSlide}
             >
                 <BsChevronCompactRight size={30} />
             </div>
 
             {/* Slide indicators */}
-            <div className='flex justify-center py-2'>
+            <div className="flex justify-center py-2">
                 {slides.map((slide, slideIndex) => (
                     <div
                         key={slideIndex}
@@ -104,4 +90,4 @@ function Epistemicon() {
     );
 }
 
-export default Epistemicon;
+export default UpwardsComponent;
